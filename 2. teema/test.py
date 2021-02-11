6c231a42433cf4435d4cb32dfb21c7499faa6c2d
Anna Karutina <anna.karutina@khk.ee>
2021-02-11 15:17:59 +0200

    Loodud lisaeksemplaarid Restoraan klassi põhjal koos kirjeldusega

--- 2. teema/test.py
+++ 2. teema/test.py
@@ -8,3 +8,17 @@ restoraan1.soogi_tyyp = "burgerid"
 restoraan1.restoraani_kirjeldus()
 restoraan1.ava_restoraan()
 
+# loome 2 eksempaari Restoraan klassi põhjal
+restoraan2 = Restoraan()
+# määra eksemplaari atribuutidele konkreetsed väärtused
+restoraan2.restoraani_nimi = "McDonalds"
+restoraan2.soogi_tyyp = "burgerid ja friikartulid"
+restoraan2.restoraani_kirjeldus()
+
+# loome 3 eksempaari Restoraan klassi põhjal
+restoraan3 = Restoraan()
+# määra eksemplaari atribuutidele konkreetsed väärtused
+restoraan3.restoraani_nimi = "Kartul"
+restoraan3.soogi_tyyp = "kartulid"
+restoraan3.restoraani_kirjeldus()
+
