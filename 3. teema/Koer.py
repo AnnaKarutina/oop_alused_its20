94efff99dccf5d771a40012c0c96dc4e5c7cffa2
Anna Karutina <anna.karutina@khk.ee>
2021-02-12 11:27:05 +0200

    Klassi konstruktor loobatribuudid ja määrab neile konkreetsed väärtused

--- 3. teema/Koer.py
+++ 3. teema/Koer.py
@@ -1,10 +1,7 @@
 class Koer():
-    hyydnimi = ""
-    vanus = 0
     # konstruktor
     def __init__(self, h, v):
         self.hyydnimi = h
-        self.vanus = v
     # meetod
     def kirjeldus(self):
         print("Koer nimega {} on {} aastat vana".format(self.hyydnimi, self.vanus))
