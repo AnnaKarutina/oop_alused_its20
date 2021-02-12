1230d0e091a75488441872e71c4209c3588848ab
Anna Karutina <anna.karutina@khk.ee>
2021-02-12 10:47:05 +0200

    Ülesanne 2.5 lahendus

--- 2. teema/test.py
+++ 2. teema/test.py
@@ -1,24 +1,13 @@
-from Restoraan import Restoraan
-# loome 1 eksempaari Restoraan klassi põhjal
-restoraan1 = Restoraan()
-# määra eksemplaari atribuutidele konkreetsed väärtused
-restoraan1.restoraani_nimi = "Hesburger"
-restoraan1.soogi_tyyp = "burgerid"
-# kutsume eksemplaari meetodid tööle
-restoraan1.restoraani_kirjeldus()
-restoraan1.ava_restoraan()
-
-# loome 2 eksempaari Restoraan klassi põhjal
-restoraan2 = Restoraan()
-# määra eksemplaari atribuutidele konkreetsed väärtused
-restoraan2.restoraani_nimi = "McDonalds"
-restoraan2.soogi_tyyp = "burgerid ja friikartulid"
-restoraan2.restoraani_kirjeldus()
-
-# loome 3 eksempaari Restoraan klassi põhjal
-restoraan3 = Restoraan()
-# määra eksemplaari atribuutidele konkreetsed väärtused
-restoraan3.restoraani_nimi = "Kartul"
-restoraan3.soogi_tyyp = "kartulid"
-restoraan3.restoraani_kirjeldus()
+from Kasutaja import Kasutaja
+# 1. kasutaja
+alice = Kasutaja() # loome objekt nimega alice
+# määrame alice oma atribuutide väärtused
+alice.eesnimi = "Alice"
+alice.perenimi = "Ime"
+alice.kasutaja_nimi = "alice"
+alice.parool = "qwerty"
+# kutsume tööle alice oma meetodid
+alice.kirjelda_kasutaja()
+print()
+alice.tervita_kasutaja()
 
