bcd7bee0e8da5b02d5246cb4e5a11d1cb29e5463
Anna Karutina <anna.karutina@khk.ee>
2021-02-12 11:31:18 +0200

    Klassi konstruktor kasutab vaikimisi väärtused

--- 3. teema/Koer.py
+++ 3. teema/Koer.py
@@ -1,8 +1,9 @@
 class Koer():
     # konstruktor
-    def __init__(self, h, v):
+    def __init__(self, h, v = 0):
         self.hyydnimi = h
         self.vanus = v
+        self.saba = True
     # meetod
     def kirjeldus(self):
         print("Koer nimega {} on {} aastat vana".format(self.hyydnimi, self.vanus))
