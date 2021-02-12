e0be1e64125d29e017cd5018a2f8da205295e4c7
Anna Karutina <anna.karutina@khk.ee>
2021-02-12 11:47:23 +0200

    Klassi destruktori kasutamine objektide eemaldamiseks

--- 3. teema/Koer.py
+++ 3. teema/Koer.py
@@ -4,6 +4,9 @@ class Koer():
         self.hyydnimi = h
         self.vanus = v
         self.saba = True
+    # destruktor
+    def __del__(self):
+        print("Kahjuks {} juba nii vana, et läheb ära".format(self.hyydnimi))
     # meetod
     def kirjeldus(self):
         print("Koer nimega {} on {} aastat vana".format(self.hyydnimi, self.vanus))
