e671c6808bd14fdf3b014b08b90c72decb7387dc
Anna Karutina <anna.karutina@khk.ee>
2021-02-16 11:02:35 +0200

    Pärilus - Auto klassi laiendamine ElektriAuto klassiga - eksemplaari loomisega tühja laiendatud klassi testimine

---
+++ 4. teema/test.py
@@ -0,0 +1,19 @@
+from Auto import Auto
+from ElektriAuto import ElektriAuto
+
+ainari_uus_auto = Auto("audi", "a6", 2017)
+
+print(ainari_uus_auto.kirjeldus())
+ainari_uus_auto.odomeeter()
+# 1 - objekti atribuuti otse väärtustamine
+# ainari_uus_auto.odomeetri_nait = 2
+ainari_uus_auto.uuenda_odomeeter(2)
+ainari_uus_auto.odomeeter()
+ainari_uus_auto.suurenda_odomeeter(30)
+ainari_uus_auto.odomeeter()
+
+tesla = ElektriAuto("tesla", "mudel s", 2019)
+print(tesla.kirjeldus())
+tesla.odomeeter()
+tesla.suurenda_odomeeter(50)
+tesla.odomeeter()
