ca788a21116a604ab1ae34513451df9780bbbf87
Anna Karutina <anna.karutina@khk.ee>
2021-02-16 09:11:08 +0200

    Kordamine - klassi atribuuti vaikimisi väärtus

--- 3. teema/test.py
+++ 3. teema/test.py
@@ -4,4 +4,7 @@ ainari_uus_auto = Auto("audi", "a6", 2017)
 minu_uus_auto = Auto("toyota", "highLander", 2021)
 
 print(ainari_uus_auto.kirjeldus())
+ainari_uus_auto.odomeeter()
+
 print(minu_uus_auto.kirjeldus())
+minu_uus_auto.odomeeter()
