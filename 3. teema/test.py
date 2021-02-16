21ae25733ea79bb58b743c691d41b097a91c3ed1
Anna Karutina <anna.karutina@khk.ee>
2021-02-16 09:04:42 +0200

    Kordamine - klassi loomine ja eksemplaari loomine klassi põhjal

--- 3. teema/test.py
+++ 3. teema/test.py
@@ -1,19 +1,7 @@
-from Koer import Koer
-nublu = Koer("Nublu", 5)
-nublu.kirjeldus()
+from Auto import Auto
 
-puppy = Koer("Puppy")
-puppy.kirjeldus()
-if(puppy.saba):
-    print("Puppy liputab")
+ainari_uus_auto = Auto("audi", "a6", 2017)
+minu_uus_auto = Auto("toyota", "highLander", 2021)
 
-vanus = 5
-while(vanus <= 15):
-    puppy.vanus = vanus
-    puppy.kirjeldus()
-    if(puppy.vanus == 15):
-        del puppy
-    vanus += 5
-input()
-
-
+print(ainari_uus_auto.kirjeldus())
+print(minu_uus_auto.kirjeldus())
