d31b673752f22e3dce72122d6ab621213a5e6ea0
Anna Karutina <anna.karutina@khk.ee>
2021-03-01 15:27:45 +0200

    Moodulid - täismooduli importimine

---
+++ 8. teema/minu_autod.py
@@ -0,0 +1,8 @@
+import auto
+#from auto import *
+
+uus_auto = auto.Auto("audi", "a6", "2017")
+print(uus_auto.kirjeldus())
+
+minu_tesla = auto.ElektriAuto('tesla', 'model s', 2016)
+print(minu_tesla.kirjeldus())
